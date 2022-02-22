178ea3306afed8ef511bb65b947e955b77c577c5
Manpapper <alexandretissier123@hotmail.fr>
2022-02-22 12:18:52 +0100

    HealthBar and more

---
+++ Assets/Scripts/Game/Player/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public CC2D playerController;
+
+    private void Start()
+    {
+        playerController = GetComponent<CC2D>();
+
+        healthBar = GetComponent<Slider>();
+        healthBar.maxValue = playerController.pStats.PlayerMaxHealth;
+        healthBar.value = playerController.pStats.PlayerHealth;
+    }
+
+    public void SetHealth(int hp)
+    {
+        healthBar.value = hp;
+    }
+}
--- Assets/Scripts/Game/Player/PlayerStats.cs
+++ Assets/Scripts/Game/Player/PlayerStats.cs
@@ -7,10 +7,12 @@ public class PlayerStats
 	int playerLvl = 0;
 	float movementSpeed = 1f;
 	float playerHealth = 100f;
+	float playerMaxHealth = 100f;
 	int playerXp = 0;
 
-	public int PlayerLvl { get => playerLvl; set => playerLvl = value; }
 	public float MovementSpeed { get => movementSpeed; set => movementSpeed = value; }
 	public float PlayerHealth { get => playerHealth; set => playerHealth = value; }
+	public float PlayerMaxHealth { get => playerMaxHealth; set => playerMaxHealth = value; }
+	public int PlayerLvl { get => playerLvl; set => playerLvl = value; }
 	public int PlayerXp { get => playerXp; set => playerXp = value; }
 }
