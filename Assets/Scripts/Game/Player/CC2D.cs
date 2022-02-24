2556f824786d6e2e5e852d0d2a2c35d06b562969
YannLescot <60198564+YannLescot@users.noreply.github.com>
2022-02-24 12:08:12 +0100

    Player attack finalized

--- Assets/Scripts/Game/Player/CC2D.cs
+++ Assets/Scripts/Game/Player/CC2D.cs
@@ -13,15 +13,20 @@ public class CC2D : MonoBehaviour
 	private HealthBar healthBar;
 	private Animator playerAnim;
 	[SerializeField]
+
 	private bool isAttacking = false;
+	private bool finishedAttacking = true;
+	private bool didFlip = true;
+
 	private float fireRate = 1f;
 	private float atkAnimSpeed = 2f;
 
-	private CircleCollider2D weaponCollider;
+	private CapsuleCollider2D weaponCollider;
 
 	private void Awake()
 	{
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		weaponCollider = this.GetComponent<CapsuleCollider2D>();
 		playerAnim = this.gameObject.GetComponent<Animator>();
 		playerAnim.SetFloat("atkAnimSpeed", atkAnimSpeed);
 	}
@@ -35,6 +40,7 @@ public class CC2D : MonoBehaviour
 		playerAttack();
 	}
 
+
 	public void Move(float moveX, float moveY)
 	{
 		// Move the character by finding the target velocity
@@ -43,13 +49,13 @@ public class CC2D : MonoBehaviour
 		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
 		// If the input is moving the player right and the player is facing left...
-		if (moveX > 0 && !m_FacingRight)
+		if (moveX > 0 && !m_FacingRight || !didFlip)
 		{
 			// ... flip the player.
 			Flip();
 		}
 		// Otherwise if the input is moving the player left and the player is facing right...
-		else if (moveX < 0 && m_FacingRight)
+		else if (moveX < 0 && m_FacingRight || !didFlip)
 		{
 			// ... flip the player.
 			Flip();
@@ -58,7 +64,8 @@ public class CC2D : MonoBehaviour
 
 	void playerAttack()
 	{
-		if (!isAttacking)
+
+		if (finishedAttacking)
 		{
 			StartCoroutine(playerAttackLogic());
 		}
@@ -66,22 +73,37 @@ public class CC2D : MonoBehaviour
 
 	IEnumerator playerAttackLogic()
 	{
-		isAttacking = false;
-		playerAnim.SetBool("isAttacking", false);
+		startAtk();
+		yield return new WaitForSeconds(1 / atkAnimSpeed);
+		stopAtk();
 		yield return new WaitForSeconds(fireRate);
+		finishedAttacking = true;
+	}
+
+	private void startAtk()
+	{
+		finishedAttacking = false;
 		playerAnim.SetBool("isAttacking", true);
 		isAttacking = true;
-		yield return new WaitForSeconds(fireRate);
+		weaponCollider.enabled = true;
+	}
+
+	private void stopAtk()
+	{
+		weaponCollider.enabled = false;
+		playerAnim.SetBool("isAttacking", false);
+		isAttacking = false;
 	}
 
 	private void Flip()
 	{
-		// Switch the way the player is labelled as facing.
-		m_FacingRight = !m_FacingRight;
-
+		didFlip = false;
 		if (!isAttacking)
 		{
+			// Switch the way the player is labelled as facing.
+			m_FacingRight = !m_FacingRight;
 			// Multiply the player's x local scale by -1.
+			didFlip = true;
 			Vector3 theScale = transform.localScale;
 			theScale.x *= -1;
 			transform.localScale = theScale;
